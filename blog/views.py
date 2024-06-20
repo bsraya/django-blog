cce321425f38df29140e82de113437a1316c658d
Bijon Setyawan Raya <bijonsetyawan@gmail.com>
2024-06-20 22:54:30 +0800

    feat: use html templating

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,22 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
-# Create your views here.
+posts = [
+    {
+        'author': 'CoreyMS',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'August 27, 2018'
+    },
+    {
+        'author': 'Jane Doe',
+         'title': 'Blog Post 2',
+         'content': 'Second post content',
+         'date_posted': 'August 28, 2018'
+    }
+]
+
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    return render(request, 'blog/index.html', {'posts': posts})
 
 def about(request):
-    return HttpResponse("Hello. This is the about page.")
+    return render(request, 'blog/about.html', {'title': 'About'})
